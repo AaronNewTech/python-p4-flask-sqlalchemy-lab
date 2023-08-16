356879f3dec465e4cfcb572e67e81770bce4c92b
Aaron <arnhsmith@gmail.com>
2023-08-15 22:12:53 -0400

    passed all tests

--- server/app.py
+++ server/app.py
@@ -1,34 +1,88 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
+
+# Configuration for the database URI and to disable modification tracking
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize the Flask-Migrate extension to handle database migrations
 migrate = Migrate(app, db)
 
+# Initialize the Flask-SQLAlchemy extension with the Flask app
 db.init_app(app)
 
 @app.route('/')
 def home():
+    # Simple home page route
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    # Query the Animal with the specified ID from the database
+    animal = Animal.query.filter(Animal.id == id).first()
+    
+    if animal:
+        # Prepare the response body with Animal information
+        response_body = f'''
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        '''
+    
+    else:
+        response_body = '<h1>404 animal not found</h1>'
+    response = make_response(response_body)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    # Query the Zookeeper with the specified ID from the database
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    
+    if zookeeper:
+        # Prepare the response body with Zookeeper information and their animals
+        response_body = f'''
+            <ul>ID: {zookeeper.id}</ul>
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+        '''
+        for animal in zookeeper.animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+        
+        response_body += '</ul></ul>'
+    else:
+        response_body = '<h1>404 zookeeper not found</h1>'
+    
+    response = make_response(response_body)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    # Query the Enclosure with the specified ID from the database
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    if enclosure:
+        # Prepare the response body with Enclosure information and the animals in it
+        response_body = f'''
+            <ul>ID: {enclosure.id}</ul>
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        '''
+        for animal in enclosure.animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+        
+        response_body += '</ul></ul>'
+    else:
+        response_body = '<h1>404 enclosure not found</h1>'
+    
+    response = make_response(response_body)
+    return response
 
 if __name__ == '__main__':
+    # Run the Flask app on port 5555 in debug mode
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,51 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy.orm import relationship
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Zookeeper(db.Model):
+    # creates table for zookeeper
     __tablename__ = 'zookeepers'
-
+    
+    # creates columns in zookeeper table for id, name and birthday
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    
+    # creates a relationship between two models and connects them to each other. 
+    # syntax plural child variable = db.relationship('child Class', backreference='parent table name')
+    animals = db.relationship('Animal', backref='zookeeper')
+    
 
 class Enclosure(db.Model):
+    # creates table for enclosure
     __tablename__ = 'enclosures'
 
+    # creates columns in enclosure table for id, environment, and open for visitors
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    
+    # creates a relationship between two models and connects them to each other. 
+    # syntax plural child variable = db.relationship('child Class', backreference='parent table name')
+    animals = db.relationship('Animal', backref='enclosure')
+
 
 class Animal(db.Model):
+    # creates table for animal
     __tablename__ = 'animals'
 
+    # creates columns for animal table for id, name and species
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    # creates a foreign key to connect to zookeeper database that goes in the animal database
+    # this is a child of the Zookeeper class
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    
+    # creates a foreign key to connect to enclosure database that goes in the animal database
+    # this is a child of the Enclosure class
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    
+
